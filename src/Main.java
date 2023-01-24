caea9f95aee6d34ccf460c65c3e8cd62bceac87d
OSS37 <suhanova375@gmail.com>
2023-01-24 13:32:09 +0300

    Homework_10 Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName+firstName+middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("Задача 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        fullName2 = fullName2.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName2);
     }
 }
